e60cd7f001a308122c60384379e091c68878fee2
sreerag <msratofficial@gmail.com>
2023-07-22 19:59:51 +0530

    feat:add email js

--- src/components/main/contact.tsx
+++ src/components/main/contact.tsx
@@ -1,7 +1,35 @@
 import { useNavContext } from '#/context/nav.context'
+import { messageType } from '#/types/message.type'
+import emailjs from '@emailjs/browser'
+import { useState } from 'react'
 
 const Contact = () => {
     const { ref4 } = useNavContext()
+    const [send, setSend] = useState<boolean>(false)
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const form = e.target as HTMLFormElement
+        const data = new FormData(form)
+        const temp: messageType = {
+            name: data.get('name'),
+            email: data.get('email'),
+            reply_to: data.get('email'),
+            message: data.get('message'),
+        }
+        console.log(temp)
+        try {
+            const res = await emailjs.send(
+                process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || '',
+                process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || '',
+                temp,
+                process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+            )
+            if (res.status == 200) setSend(true)
+        } catch (error) {
+            setSend(false)
+        }
+    }
     return (
         <section ref={ref4} data-id="section4" id="contact">
             <div className="container space-y-3 text-white lg:space-y-4 ">
@@ -14,6 +42,7 @@ const Contact = () => {
                 <form
                     className=" min-w-md space-y-8 uppercase  "
                     autoComplete="off"
+                    onSubmit={handleSubmit}
                 >
                     <div className="flex flex-col space-y-4">
                         <label htmlFor="name" className="">
@@ -22,6 +51,7 @@ const Contact = () => {
                         <input
                             type="text"
                             id="name"
+                            name="name"
                             className="min-h-[40px] rounded border-none bg-black_grey p-3 outline-none
                             hover:ring-offset-[0.25px] hover:ring-offset-white md:rounded-lg lg:ring-0
                              lg:ring-offset-0 lg:ring-offset-black
@@ -35,6 +65,7 @@ const Contact = () => {
                         <input
                             type="email"
                             id="email"
+                            name="email"
                             className=" min-h-[40px] rounded border-none bg-black_grey p-3 outline-none
                             hover:ring-offset-[0.25px] hover:ring-offset-white md:rounded-lg lg:ring-0
                              lg:ring-offset-0 lg:ring-offset-black
@@ -49,6 +80,7 @@ const Contact = () => {
                         <textarea
                             id="message"
                             rows={4}
+                            name="message"
                             className="glass min-h-[140px] resize-none rounded border-none bg-black_grey p-3 outline-none"
                         ></textarea>
                     </div>
@@ -67,7 +99,7 @@ const Contact = () => {
                               lg:ring-offset-0
                              "
                         >
-                            Send
+                            {send ? 'Done' : 'Send'}
                         </button>
                     </div>
                 </form>
---
+++ src/types/message.type.ts
@@ -0,0 +1,6 @@
+export type messageType = {
+    name: FormDataEntryValue | string | null
+    email: FormDataEntryValue | string | null
+    message: FormDataEntryValue | string | null
+    reply_to: FormDataEntryValue | string | null
+}
